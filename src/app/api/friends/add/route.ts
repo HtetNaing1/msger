6db62e444b0439acf5e49880f232ff3f60f5c000
Htet <htetnaing.chess@gmail.com>
2023-06-20 12:42:26 +0630

    add pusher for real time communication

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -4,6 +4,8 @@ import { db } from "@/lib/db";
 import { addFriendValidator } from "@/lib/validations/add-friend";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
+import { toPusherKey } from "@/lib/utils";
+import { pusherServer } from "@/lib/pusher";
 
 export async function POST(req: Request) {
   try {
@@ -55,6 +57,15 @@ export async function POST(req: Request) {
     }
 
     // valid request, send friend request
+
+    await pusherServer.trigger(
+      toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
+      "incoming_friend_requests",
+      {
+        senderId: session.user.id,
+        senderEmail: session.user.email,
+      }
+    );
 
     await db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
 
