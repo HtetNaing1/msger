66d08d7ce56af08c2a00561f08eefb0a6588a07b
Htet <htetnaing.chess@gmail.com>
2023-06-27 14:38:18 +0630

    Create notification

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -5,6 +5,7 @@ import { chatHrefConstructor, toPusherKey } from "@/lib/utils";
 import { usePathname, useRouter } from "next/navigation";
 import { FC, useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
+import UnseenChatToast from "./UnseenChatToast";
 
 interface SidebarChatListProps {
   friends: User[];
@@ -31,11 +32,36 @@ const SidebarChatList: FC<SidebarChatListProps> = ({ friends, sessionId }) => {
       setActiveChats((prev) => [...prev, newFriend]);
     };
 
+    const chatHandler = (message: ExtendedMessage) => {
+      const shouldNotify =
+        pathname !==
+        `/dashboard/chat/${chatHrefConstructor(sessionId, message.senderId)}`;
+
+      if (!shouldNotify) return;
+
+      // should be notified
+      toast.custom((t) => (
+        <UnseenChatToast
+          t={t}
+          sessionId={sessionId}
+          senderId={message.senderId}
+          senderImg={message.senderImg}
+          senderMessage={message.text}
+          senderName={message.senderName}
+        />
+      ));
+
+      setUnseenMessages((prev) => [...prev, message]);
+    };
+
+    pusherClient.bind("new_message", chatHandler);
     pusherClient.bind("new_friend", newFriendHandler);
 
     return () => {
+      pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:chats`));
       pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`));
 
+      pusherClient.unbind("new_message", chatHandler);
       pusherClient.unbind("new_friend", newFriendHandler);
     };
   }, [pathname, sessionId, router]);
